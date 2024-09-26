96ebcf3da9328aec3e3d28892d5e40ff9e950955
musimarcin <marcinm994@gmail.com>
2024-09-26 22:10:04 +0200

    added bootstrap, fetching data from docker and listing it on page

--- src/main/java/com/movies_api/api/MovieController.java
+++ src/main/java/com/movies_api/api/MovieController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/movies")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class MovieController {
 
     @Autowired
