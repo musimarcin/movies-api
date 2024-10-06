7938a35dc293e44afa890edc8b525cc8f896be50
musimarcin <marcinm994@gmail.com>
2024-10-06 12:21:36 +0200

    changed enviromental variable name

--- movies-api-ui/src/app/components/api.ts
+++ movies-api-ui/src/app/components/api.ts
@@ -2,15 +2,15 @@ import axios, {AxiosResponse} from "axios";
 import {MovieResponse} from "./Movies";
 
 export const getApiUrl = () => {
-    let apiUrl;
-
+//     let apiUrl;
+//
 //     // Assuming a simple check for local environment
 //     if (typeof window === 'undefined') { // Running on the server
 //         apiUrl = process.env.NEXT_PUBLIC_SERVER_URL;
 //     } else { // Running on the client
 //         apiUrl = process.env.NEXT_PUBLIC_CLIENT_URL;
 //     }
-    apiUrl = process.env.NEXT_PUBLIC_SERVER_URL
+    const apiUrl = process.env.API_URL
     if (!apiUrl) {
         console.error('API URL is not defined');
         throw new Error('API URL is not defined');
