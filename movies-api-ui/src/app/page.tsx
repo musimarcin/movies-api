6a4b13320f3c821deb6039848b8aad1a6654bf13
musimarcin <marcinm994@gmail.com>
2024-10-12 10:15:18 +0200

    removed unnecessary api and homemovie files, fixed fetching data server-side with api route handler and pagination

--- movies-api-ui/src/app/page.tsx
+++ movies-api-ui/src/app/page.tsx
@@ -14,32 +14,33 @@ export default function Home() {
   const [totalPages, setTotalPages] = useState<number>(1); //
 
   // fetching movies with route.ts in api/movies/
-//     try {
     const fetchMovies = async () => {
-      const res = await fetch(`/api/movies?page=${page}`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        cache: 'no-store',
-      });
-      const data = await res.json();
-       console.log(data)
+        try {
+          const res = await fetch(`/api?page=${page}`, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            cache: 'no-store',
+          });
+          const data = await res.json();
+          console.log(data)
 
-    if (!res.ok) throw new Error("Failed to load movies")
+        if (!res.ok) throw new Error("Failed to load movies")
 
-      setMovies(data.movieList.movieList);
-      setTotalPages(data.totalPages);
+        setMovies(data.movieList.movieList);
+        setTotalPages(data.movieList.totalPages);
 
-//     } catch (error) {
-//         if (error instanceof Error) {
-//             console.error('Fetch error:', error.message);
-//             setError(error.message);
-//         } else {
-//             console.error('An unknown error occurred:', error);
-//         }
-//     };
-    }
+        } catch (error) {
+            if (error instanceof Error) {
+                console.error('Fetch error:', error.message);
+                setError(error.message);
+            } else {
+                console.error('An unknown error occurred:', error);
+            }
+        }
+    };
+
   useEffect(() => {
     fetchMovies();
   }, [page]);
