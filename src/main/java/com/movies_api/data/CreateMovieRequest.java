0002cd67ea5b9a276b6d012243e484439de917d7
musimarcin <marcinm994@gmail.com>
2024-10-19 13:21:06 +0200

    created user entity, repo, service and controller also resource exception

--- src/main/java/com/movies_api/data/CreateMovieRequest.java
+++ src/main/java/com/movies_api/data/CreateMovieRequest.java
@@ -9,7 +9,7 @@ import lombok.Setter;
 @Setter
 public class CreateMovieRequest {
     //this class is used instead of MovieDTO because request will need title and release year instead of all Movie properties
-    //handling errors of missing fields
+    //handling errors of missing fieldss
     @NotEmpty(message = "Fill in title")
     private String title;
     @NotNull(message = "Fill in release year")
